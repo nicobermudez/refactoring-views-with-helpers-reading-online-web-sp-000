7e1a57bed0a263109100c6aa777cd005ba14557e
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-20 16:05:34 +0000

    Automatically backed up by Learn

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,7 @@
 module PostsHelper
 
   def last_updated(post)
-    updated_at.strftime("Last updated %A, %b %e, at %1:%< %p")
+    post.updated_at.strftime("Last updated %A, %b %e, at %1:%< %p")
   end
 
 end
